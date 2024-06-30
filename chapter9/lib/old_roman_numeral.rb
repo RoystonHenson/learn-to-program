10ef7f3fbd4759516e50ded590ba328446a502ac
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-30 14:03:26 +0100

    Add ability for user to enter number 1, have it converted to roman numeral and printed for user to see

--- chapter9/lib/old_roman_numeral.rb
+++ chapter9/lib/old_roman_numeral.rb
@@ -1,3 +1,15 @@
 class OldRomanNumeral
+  attr_reader :number, :rom_number
 
+  def get_num
+    @number = $stdin.gets.chomp
+  end
+
+  def num_to_rom(number)
+     @rom_number = 'I' * number
+  end
+
+  def print_num_to_rom
+    puts rom_number
+  end
 end
--- chapter9/spec/old_roman_numberal_spec.rb
+++ chapter9/spec/old_roman_numberal_spec.rb
@@ -1,4 +1,31 @@
 require 'old_roman_numeral'
 
+describe OldRomanNumeral do
+  let(:orn) { described_class.new }
 
+  describe '#get_num' do
+  let(:number) { StringIO.new('1')}
+    it 'sets number provded by user to a variable' do
+      $stdin = number
+      orn.get_num
+      expect(orn.number).to eq('1')
+      $stdin = STDIN
+    end
+  end
+
+  describe '#num_to_rom' do
+    it 'converts number 1 to old roman equivalent for 1' do
+      
+      num = 1
+      expect(orn.num_to_rom(num)).to eq('I')
+    end
+  end
+
+  describe '#print_num_to_rom' do
+    it 'prints roman numeral for user to see' do
+      num = 1
+      orn.num_to_rom(num)
+      expect { orn.print_num_to_rom }.to output("I\n").to_stdout
+    end
+  end
 end
