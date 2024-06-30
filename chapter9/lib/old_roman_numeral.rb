904ee319c6a96f6c50a7a533e13bd3c2fe083776
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-06-30 23:27:56 +0100

    Add conversion to roman numerals

--- chapter9/lib/old_roman_numeral.rb
+++ chapter9/lib/old_roman_numeral.rb
@@ -1,12 +1,28 @@
 class OldRomanNumeral
   attr_reader :number, :rom_number
 
+  def initialize
+    @rom_number = ''
+  end
+
   def get_num
     @number = $stdin.gets.chomp
   end
 
   def num_to_rom(number)
-     @rom_number = 'I' * number
+      rom_number << 'M' * (number / 1000)
+      number %= 1000
+      rom_number << 'D' * (number / 500)
+      number %= 500
+      rom_number << 'C' * (number / 100)
+      number %= 100
+      rom_number << 'L' * (number / 50)
+      number %= 50
+      rom_number << 'X' * (number / 10)
+      number %= 10
+      rom_number << 'V' * (number / 5)
+      number %= 5
+    rom_number << 'I' * number
   end
 
   def print_num_to_rom
--- chapter9/spec/old_roman_numberal_spec.rb
+++ chapter9/spec/old_roman_numberal_spec.rb
@@ -3,6 +3,12 @@ require 'old_roman_numeral'
 describe OldRomanNumeral do
   let(:orn) { described_class.new }
 
+  describe '#initialize' do
+    it 'initialises with rom_number variable' do
+      expect(orn).to respond_to(:rom_number)
+    end
+  end
+
   describe '#get_num' do
   let(:number) { StringIO.new('1')}
     it 'sets number provded by user to a variable' do
@@ -14,10 +20,39 @@ describe OldRomanNumeral do
   end
 
   describe '#num_to_rom' do
-    it 'converts number 1 to old roman equivalent for 1' do
-      
+    it 'converts number 1 to I' do
       num = 1
       expect(orn.num_to_rom(num)).to eq('I')
+    end
+
+    it 'converts number 6 to VI' do
+      num = 6
+      expect(orn.num_to_rom(num)).to eq('VI')
+    end
+
+    it 'converts number 17 to XVI' do
+      num = 17
+      expect(orn.num_to_rom(num)).to eq('XVII')
+    end
+    
+    it 'converts number 58 to LVI' do
+      num = 58
+      expect(orn.num_to_rom(num)).to eq('LVIII')
+    end
+
+    it 'converts number 169 to CLXVIIII' do
+      num = 169
+      expect(orn.num_to_rom(num)).to eq('CLXVIIII')
+    end
+
+    it 'converts number 576 to DLXXVI' do
+      num = 576
+      expect(orn.num_to_rom(num)).to eq('DLXXVI')
+    end
+
+    it 'converts 1687 to MDCLXXXVII' do
+      num = 1687
+      expect(orn.num_to_rom(num)).to eq('MDCLXXXVII')
     end
   end
 
