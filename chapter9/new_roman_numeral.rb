b6830e0e7ca1f0cb8da63febba6840f888bb44da
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-01 14:35:04 +0100

    Add ability to use new roman conversion for '90..99', '40..499' and '900..999'

--- chapter9/new_roman_numeral.rb
+++ chapter9/new_roman_numeral.rb
@@ -1,15 +1,35 @@
 def num_to_rom(number)
   @rom_number = ''
   number = convert_num_to_rom('M', 1000, number)
-  number = convert_num_to_rom('D', 500, number)
-  number = convert_num_to_rom('C', 100, number)
-  number = convert_num_to_rom('L', 50, number)
+  
+  if (number - 900) < 100 && (number - 900) >= 0
+    @rom_number << 'CM'
+    number -= 900
+  else
+    number = convert_num_to_rom('D', 500, number)
+  end
+
+  if (number - 400) < 100 && (number - 400) >= 0
+    @rom_number << 'CD'
+    number -= 400
+  else
+    number = convert_num_to_rom('C', 100, number)
+  end
+
+  if (number - 90) < 10 && (number - 90) >= 0
+    @rom_number << 'XC'
+    number -= 90
+  else
+    number = convert_num_to_rom('L', 50, number)
+  end
+
   if (number - 40) < 10 && (number - 40) >= 0
     @rom_number << 'XL'
     number -= 40
   else
     number = convert_num_to_rom('X', 10, number)
   end
+
   if number == 9
     @rom_number << 'IX'
   else
@@ -23,27 +43,21 @@ def convert_num_to_rom(character, divisor, number)
   number %= divisor
 end
 
-num_to_rom(39)
-puts @rom_number
-num_to_rom(40)
-puts @rom_number
-num_to_rom(41)
-puts @rom_number
-num_to_rom(42)
-puts @rom_number
-num_to_rom(43)
-puts @rom_number
-num_to_rom(44)
-puts @rom_number
-num_to_rom(45)
-puts @rom_number
-num_to_rom(46)
-puts @rom_number
-num_to_rom(47)
-puts @rom_number
-num_to_rom(48)
-puts @rom_number
-num_to_rom(49)
-puts @rom_number
-num_to_rom(50)
-puts @rom_number
+def test(num)
+  puts num_to_rom(num)
+end
+
+test(899)
+test(900)
+test(901)
+test(912)
+test(923)
+test(934)
+test(945)
+test(956)
+test(967)
+test(978)
+test(988)
+test(999)
+test(1000)
+test(1001)
