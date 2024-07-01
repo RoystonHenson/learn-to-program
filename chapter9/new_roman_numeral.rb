e812e2f63e0f5e03820b2e1cbe1fb7b4d52ae9a6
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-01 14:50:54 +0100

    Comment out tests, change method name

--- chapter9/new_roman_numeral.rb
+++ chapter9/new_roman_numeral.rb
@@ -1,48 +1,49 @@
 def num_to_rom(number)
   @rom_number = ''
-  number = convert_num_to_rom('M', 1000, number)
+  number = convert_additive_num('M', 1000, number)
   
   if (number - 900) < 100 && (number - 900) >= 0
     @rom_number << 'CM'
     number -= 900
   else
-    number = convert_num_to_rom('D', 500, number)
+    number = convert_additive_num('D', 500, number)
   end
 
   if (number - 400) < 100 && (number - 400) >= 0
     @rom_number << 'CD'
     number -= 400
   else
-    number = convert_num_to_rom('C', 100, number)
+    number = convert_additive_num('C', 100, number)
   end
 
   if (number - 90) < 10 && (number - 90) >= 0
     @rom_number << 'XC'
     number -= 90
   else
-    number = convert_num_to_rom('L', 50, number)
+    number = convert_additive_num('L', 50, number)
   end
 
   if (number - 40) < 10 && (number - 40) >= 0
     @rom_number << 'XL'
     number -= 40
   else
-    number = convert_num_to_rom('X', 10, number)
+    number = convert_additive_num('X', 10, number)
   end
 
   if number == 9
     @rom_number << 'IX'
   else
-    number = convert_num_to_rom('V', 5, number)
+    number = convert_additive_num('V', 5, number)
     (number + 1) % 5 == 0 ? @rom_number << 'IV' : @rom_number << 'I' * number
   end
 end
 
-def convert_num_to_rom(character, divisor, number)
+def convert_additive_num(character, divisor, number)
   @rom_number << character * (number / divisor)
   number %= divisor
 end
 
+=begin
 def test(num)
   puts num_to_rom(num)
 end
@@ -61,3 +62,4 @@ test(988)
 test(999)
 test(1000)
 test(1001)
+=end
