f2d727835ce56693dac48fc6324e281be634bdbc
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-02 15:35:34 +0100

    Add ability to 'ask for number', 'save number', 'print number' and 'convert number 1'

--- chapter10/lib/english_number.rb
+++ chapter10/lib/english_number.rb
@@ -1,3 +1,25 @@
 class EnglishNumber
+    attr_reader :number
 
+  def prompt_user
+    print 'Hello! Please enter a number: '
+    save_number($stdin.gets.chomp)
+  end
+
+  def save_number(number)
+    @number = number
+  end
+
+  def num_to_eng(number)
+    if number == '1'
+      @neg_num = 'One'
+    else
+      puts 'something went wrong'
+    end
+  end
+
+  def print_number
+    print @number
+  end
+  
 end
--- chapter10/spec/english_number_spec.rb
+++ chapter10/spec/english_number_spec.rb
@@ -3,4 +3,26 @@ require 'english_number'
 describe EnglishNumber do
   let(:en) { described_class.new }
 
+  describe '#prompt_user' do
+    it 'asks user to input number' do
+      expect { en.prompt_user }.to output('Hello! Please enter a number: ').to_stdout
+    end
+  end
+
+  describe '#save_number' do
+    let(:io) { StringIO.new ('1')}
+    it 'saves users number' do
+      $stdin = io.gets
+      en.save_number($stdin)
+      expect(en.number).to eq('1')
+      $stdin = STDIN
+    end
+
+    describe '#num_to_eng' do
+      it "converts user's number to english" do
+        number = en.save_number('1')
+        expect(en.num_to_eng(number)).to eq('One')
+      end
+    end
+  end
 end
