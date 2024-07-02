b1dae2d61bffe44a92572b64939425f78a225139
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-02 17:55:18 +0100

    Add ability to convert numbers 0 through 10

--- chapter10/lib/english_number.rb
+++ chapter10/lib/english_number.rb
@@ -1,25 +1,34 @@
 class EnglishNumber
-    attr_reader :number
-
+    attr_reader :number, :eng_num
+  
   def prompt_user
     print 'Hello! Please enter a number: '
     save_number($stdin.gets.chomp)
   end
 
   def save_number(number)
-    @number = number
+    @number = number.to_i
   end
 
   def num_to_eng(number)
-    if number == '1'
-      @neg_num = 'One'
-    else
-      puts 'something went wrong'
+    case number
+      when 0 then @eng_num = 'zero'
+      when 1 then @eng_num = 'one' 
+      when 2 then @eng_num = 'two'
+      when 3 then @eng_num = 'three'
+      when 4 then @eng_num = 'four'
+      when 5 then @eng_num = 'five'
+      when 6 then @eng_num = 'six'
+      when 7 then @eng_num = 'seven'
+      when 8 then @eng_num = 'eight'
+      when 9 then @eng_num = 'nine'
+      when 10 then @eng_num = 'ten' 
     end
+    @eng_num.capitalize!
   end
 
   def print_number
-    print @number
+    print "#{number}? I think you mean #{eng_num}!"
   end
   
 end
--- chapter10/spec/english_number_spec.rb
+++ chapter10/spec/english_number_spec.rb
@@ -14,14 +14,34 @@ describe EnglishNumber do
     it 'saves users number' do
       $stdin = io.gets
       en.save_number($stdin)
-      expect(en.number).to eq('1')
+      expect(en.number).to eq(1)
       $stdin = STDIN
     end
 
     describe '#num_to_eng' do
-      it "converts user's number to english" do
-        number = en.save_number('1')
-        expect(en.num_to_eng(number)).to eq('One')
+      it "Converts '0' to 'Zero'" do
+        number = en.save_number('0')
+        expect(en.num_to_eng(number)).to eq('Zero')
+      end
+
+      it "converts '9' to 'Nine" do
+        number = en.save_number('9')
+        expect(en.num_to_eng(number)).to eq('Nine')
+      end
+
+      it "converts '10' to 'Ten" do
+        number = en.save_number('10')
+        expect(en.num_to_eng(number)).to eq('Ten')
+      end
+    end
+
+    describe '#print_number' do
+      it 'prints converted number for user' do
+        en.save_number('1')
+        p en.number
+        en.num_to_eng(en.number)
+        p en.eng_num
+        expect { en.print_number }.to output("#{en.number}? I think you mean #{en.eng_num}!").to_stdout 
       end
     end
   end
