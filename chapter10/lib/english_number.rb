4ba3cf6b0ebc149673a67cada4d86392a318f1e3
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-02 22:57:50 +0100

    Add ability to convert from 11-100

--- chapter10/lib/english_number.rb
+++ chapter10/lib/english_number.rb
@@ -11,20 +11,86 @@ class EnglishNumber
   end
 
   def num_to_eng(number)
-    case number
-      when 0 then @eng_num = 'zero'
-      when 1 then @eng_num = 'one' 
-      when 2 then @eng_num = 'two'
-      when 3 then @eng_num = 'three'
-      when 4 then @eng_num = 'four'
-      when 5 then @eng_num = 'five'
-      when 6 then @eng_num = 'six'
-      when 7 then @eng_num = 'seven'
-      when 8 then @eng_num = 'eight'
-      when 9 then @eng_num = 'nine'
-      when 10 then @eng_num = 'ten' 
+    if number == 100
+      @eng_num = 'one-hundred'
+    elsif number / 10 == 9 && number % 10 != 0
+      @eng_num = "ninety-#{one_to_nine(number - 90)}"
+    elsif number == 90
+      @eng_num = 'ninety'
+    elsif number / 10 == 8 && number % 10 != 0
+      @eng_num = "eighty-#{one_to_nine(number - 80)}"
+    elsif number == 80
+      @eng_num = 'eighty'
+    elsif number / 10 == 7 && number % 10 != 0
+      @eng_num = "seventy-#{one_to_nine(number - 70)}"
+    elsif number == 70
+      @eng_num = 'seventy'
+    elsif number / 10 == 6 && number % 10 != 0
+      @eng_num = "sixty-#{one_to_nine(number - 60)}"
+    elsif number == 60
+      @eng_num = 'sixty'
+    elsif number / 10 == 5 && number % 10 != 0
+      @eng_num = "fifty-#{one_to_nine(number - 50)}"
+    elsif number == 50
+      @eng_num = 'fifty'
+    elsif number / 10 == 4 && number % 10 != 0
+      @eng_num = "fourty-#{one_to_nine(number - 40)}"
+    elsif number == 40
+      @eng_num = 'fourty'
+    elsif number / 10 == 3 && number % 10 != 0
+      @eng_num = "thirty-#{one_to_nine(number - 30)}"
+    elsif number == 30
+      @eng_num = 'thirty'
+    elsif number / 10 == 2 && number % 10 != 0
+      @eng_num = "twenty-#{one_to_nine(number - 20)}"
+    elsif number == 20
+      @eng_num = 'twenty'
+    elsif number == 19   
+      @eng_num = 'nineteen'
+    elsif number == 18
+      @eng_num = 'eighteen'
+    elsif number == 17
+      @eng_num = 'seventeen'
+    elsif number == 16
+      @eng_num = 'sixteen'
+    elsif number == 15
+      @eng_num = 'fifteen'
+    elsif number == 14
+      @eng_num = 'fourteen'
+    elsif number == 13
+      @eng_num = 'thirteen'
+    elsif number == 12
+      @eng_num = 'twelve'
+    elsif number == 11
+      @eng_num = 'eleven'
+    elsif number == 10
+      @eng_num = 'ten' 
     end
-    @eng_num.capitalize!
+    one_to_nine(number)  
+    @eng_num = 'zero' if number == 0 
+    @eng_num
+  end
+
+  def one_to_nine(number)
+    if number == 9
+      @eng_num = 'nine' 
+    elsif number == 8
+      @eng_num = 'eight'
+    elsif number == 7
+      @eng_num = 'seven'
+    elsif number == 6
+      @eng_num = 'six'
+    elsif number == 5
+      @eng_num = 'five'
+    elsif number == 4 
+      @eng_num = 'four'
+    elsif number == 3
+      @eng_num = 'three'
+    elsif number == 2
+      @eng_num = 'two'
+    elsif number == 1
+      @eng_num = 'one'
+    end
   end
 
   def print_number
--- chapter10/spec/english_number_spec.rb
+++ chapter10/spec/english_number_spec.rb
@@ -19,28 +19,66 @@ describe EnglishNumber do
     end
 
     describe '#num_to_eng' do
-      it "Converts '0' to 'Zero'" do
-        number = en.save_number('0')
-        expect(en.num_to_eng(number)).to eq('Zero')
+      it "Converts '11' to 'eleven'" do
+        number = en.save_number('11')
+        expect(en.num_to_eng(number)).to eq('eleven')
       end
 
-      it "converts '9' to 'Nine" do
-        number = en.save_number('9')
-        expect(en.num_to_eng(number)).to eq('Nine')
+      it "converts '19' to 'nineteen" do
+        number = en.save_number('19')
+        expect(en.num_to_eng(number)).to eq('nineteen')
       end
 
-      it "converts '10' to 'Ten" do
-        number = en.save_number('10')
-        expect(en.num_to_eng(number)).to eq('Ten')
+      it "converts '22' to 'twenty-two" do
+        number = en.save_number('22')
+        expect(en.num_to_eng(number)).to eq('twenty-two')
+      end
+
+      it "converts '33' to 'thirty-three" do
+        number = en.save_number('33')
+        expect(en.num_to_eng(number)).to eq('thirty-three')
+      end
+
+      it "converts '44' to 'fourty-four" do
+        number = en.save_number('44')
+        expect(en.num_to_eng(number)).to eq('fourty-four')
+      end
+
+      it "converts '55' to 'fifty-five" do
+        number = en.save_number('55')
+        expect(en.num_to_eng(number)).to eq('fifty-five')
+      end
+
+      it "converts '66' to 'sixty-six" do
+        number = en.save_number('66')
+        expect(en.num_to_eng(number)).to eq('sixty-six')
+      end
+
+      it "converts '77' to 'seventy-seven" do
+        number = en.save_number('77')
+        expect(en.num_to_eng(number)).to eq('seventy-seven')
+      end
+
+      it "converts '88' to 'eighty-eight" do
+        number = en.save_number('88')
+        expect(en.num_to_eng(number)).to eq('eighty-eight')
+      end
+      
+      it "converts '99' to 'ninety-nine" do
+        number = en.save_number('99')
+        expect(en.num_to_eng(number)).to eq('ninety-nine')
+      end
+
+      it "converts '100' to 'one-hundred" do
+        number = en.save_number('100')
+        expect(en.num_to_eng(number)).to eq('one-hundred')
       end
     end
 
     describe '#print_number' do
       it 'prints converted number for user' do
         en.save_number('1')
-        p en.number
         en.num_to_eng(en.number)
-        p en.eng_num
         expect { en.print_number }.to output("#{en.number}? I think you mean #{en.eng_num}!").to_stdout 
       end
     end
