2417721421903f1b074dcd939ff69eb43682c67b
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-07-03 19:07:02 +0100

    Add '#birthday_helper', it can tell you when the next birthday is

--- chapter12/ex12.rb
+++ chapter12/ex12.rb
@@ -17,11 +17,41 @@ end
 def happy_birthday
 puts 'What year were you born in? ' 
 year = $stdin.gets.chomp
-puts 'And what number month?'
+puts 'And what number month? '
 month = $stdin.gets.chomp
-puts 'And the number day?'
+puts 'And the number day? '
 day = $stdin.gets.chomp
 birth_date = Time.new(year, month, day)
 age = ((((Time.new - birth_date) / 60) / 60) /24) / 365
 puts "Great! Here is your present! One for each year! #{"SPANK!" * age.to_i}"
 end
+
+# Birthday Helper
+def birthday_helper
+  array = File.readlines('birthdays.txt').each_with_index
+  array.map { |x| x.gsub!(/\n/, '') }
+  array.map { |x| x.gsub!(/\t/, '') }
+  hash = {}
+  array.each { |x|
+                temp = x.split(',')
+                hash[temp[0]] = "#{temp[1]} #{temp[2]}"
+              }
+  puts 'Whose birthday would you like to view?: '
+  hash.each_key { |k| puts "  -#{k}"}
+  answer = $stdin.gets.chomp
+  if hash.has_key?(answer)
+    birth_date = hash[answer].split
+    birth_time = Time.new(birth_date[2], birth_date[0], birth_date[1])
+    birthday_this_year = Time.new(Time.new.year, birth_date[0], birth_date[1])
+    if birthday_this_year > Time.now
+      puts "Their next birthday will be on #{birthday_this_year.day}/#{birthday_this_year.month}." 
+      puts "They will be #{birthday_this_year.year - birth_time.year}!"
+    else
+      puts "Their next birthday will be next year on #{birthday_this_year.day}/#{birthday_this_year.month}/#{birthday_this_year.year + 1}."
+      puts "They will be #{(birthday_this_year.year - birth_time.year) +1}!"
+    end
+  else
+    puts "I'm sorry. That birthday is not in our database."
+  end
+end
+birthday_helper
